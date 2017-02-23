0b6e7255405ee186ce4a1aa9587ceec7b7d1995a
keunhyeok <hyc7575@naver.com>
2017-02-23 14:44:44 +0900

    (function (global) { //브라우저 별 함수가 다를 경우를 지원하기 위한 영역

--- h2m.js
+++ h2m.js
@@ -1,88 +1,148 @@
-(function() {
-  'use stric';
+(function (global) { //브라우저 별 함수가 다를 경우를 지원하기 위한 영역
+  'use strict';
 
-  this.testObj = {
-    data : {
-      0 : {
-        bName: '미미미123',
-        color: {
-          boxColor: '#fff',
-          lineColor: '#fbfbfb',
-          id: 8
-        },
-        weddingHall: '아스티에'
-      },
-      1: {
-        bName: '가이오낭',
-        color: {
-          boxColor: '#fcfcfc',
-          lineColor: '#aaa',
-          id: 12
-        },
-        weddingHall: '우디홀'
-      },
-      2: {
-        bName: '전근혁',
-        color: {
-          boxColor: '#fff',
-          lineColor: '#fbfbfb',
-          id: 6
-        },
-        weddingHall: '코르비'
-      },
-      3: {
-        bName: '하이홍',
-        color: {
-          boxColor: '#fff',
-          lineColor: '#fbfbfb',
-          id: 13
-        },
-        weddingHall: '세시코'
-      },
-      4: {
-        bName: '주노주노',
-        color: {
-          boxColor: '#fff',
-          lineColor: '#fbfbfb',
-          id: 1
-        },
-        weddingHall: '도아잉ㅇ'
-      },
-      5: {
-        bName: '코에키스',
-        color: {
-          boxColor: '#fff',
-          lineColor: '#fbfbfb',
-          id: 4
-        },
-        weddingHall: '허햐해이멘'
+  var ArrayProto = Array.prototype,
+      ObjProto = Object.prototype;
+
+  if (typeof window.console === "undefined" || typeof window.console.log === "undefined") { // ie8 console log 객체 생성 (에러 방지)
+    global.console = { log: function () { } };
+  }
+
+  if (!ArrayProto.map) {
+    ArrayProto.map = function(callback, thisArg) {
+      var T, A, k;
+      if (this == null) {
+        throw new TypeError(' this is null or not defined');
       }
-    },
-    hallMap : {
-      '무슨홀' : {
-        id: 153,
-        color: '#fbcfdc'
-      },
-      'afa홀' : {
-        id: 12,
-        color: '#fbcfdc'
-      },
-      '카이홀' : {
-        id: 168,
-        color: '#fbcfdc'
-      },
-      '제이홀' : {
-        id: 445,
-        color: '#fbcfdc'
-      },
-      '프리아미저홀' : {
-        id: 53,
-        color: '#fbcfdc'
+      var O = Object(this);
+      var len = O.length >>> 0;
+      if (typeof callback !== 'function') {
+        throw new TypeError(callback + ' is not a function');
       }
-    }
+
+      if (arguments.length > 1) {
+        T = thisArg;
+      }
+
+      A = new Array(len);
+
+      // 7. Let k be 0
+      k = 0;
+
+      while (k < len) {
+
+        var kValue, mappedValue;
+        if (k in O) {
+          kValue = O[k];
+          A[k] = mappedValue;
+        }
+        k++;
+      }
+      return A;
+    };
+  }
+
+  if (!ArrayProto.indexOf) {
+    ArrayProto.indexOf = function(searchElement, fromIndex) {
+
+      var k;
+
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      var o = Object(this);
+      var len = o.length >>> 0;
+
+      if (len === 0) {
+        return -1;
+      }
+      var n = fromIndex | 0;
+
+      if (n >= len) {
+        return -1;
+      }
+
+      k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+      // 9. k <len 인 동안 반복한다.
+      while (k < len) {
+        if (k in o && o[k] === searchElement) {
+          return k;
+        }
+        k++;
+      }
+      return -1;
+    };
+  }
+
+  if (!Array.prototype.filter) {
+    Array.prototype.filter = function(fun/*, thisArg*/) {
+
+      if (this === void 0 || this === null) {
+        throw new TypeError();
+      }
+
+      var t = Object(this);
+      var len = t.length >>> 0;
+      if (typeof fun !== 'function') {
+        throw new TypeError();
+      }
+
+      var res = [];
+      var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+      for (var i = 0; i < len; i++) {
+        if (i in t) {
+          var val = t[i];
+
+          // 주의: 기술상 이는 아마 다음 인덱스에서
+          //       Object.defineProperty일 것임, push는 Object.prototype
+          //       및 Array.prototype 상 속성에 의해 영향을 받을 수 있기에.
+          //       그러나 그 메소드는 새롭고 충돌은
+          //       드물기에 더 호환되는 대안을 쓰세요.
+          if (fun.call(thisArg, val, i, t)) {
+            res.push(val);
+          }
+        }
+      }
+
+      return res;
+    };
+  }
+
+  if (!ArrayProto.filter) {
+    ArrayProto.filter = function(fun) {
+
+      if (this === void 0 || this === null) {
+        throw new TypeError();
+      }
+
+      var t = Object(this);
+      var len = t.length >>> 0;
+      if (typeof fun !== 'function') {
+        throw new TypeError();
+      }
+
+      var res = [];
+      var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+      for (var i = 0; i < len; i++) {
+        if (i in t) {
+          var val = t[i];
+
+          if (fun.call(thisArg, val, i, t)) {
+            res.push(val);
+          }
+        }
+      }
+
+      return res;
+    };
   }
 
 
+})(window);
+(function() {
+  'use stric';
 
   var global = this;
 
@@ -102,6 +162,11 @@
   }
   h2m.addComma = addComma;
   h2m.removeComma = removeComma;
+
+  function onlyNumber(val) {
+    return val.replace(/[^0-9\-]/gi,'');
+  }
+  h2m.onlyNumber = onlyNumber;
 
   // compare callback function 기본으로 적용된 sort
   function sort(array, order) {
@@ -265,7 +330,7 @@
   function isArray(array) {
     return Object.prototype.toString.call(array) === '[object Array]';
   }
-  
+
   h2m.arrayIntersect = arrayIntersect;
   h2m.arrayIndexOf = arrayIndexOf;
   h2m.getShortArray = getShortArray;

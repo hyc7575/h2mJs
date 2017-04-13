a1de7e14e2d8e14678af1d994accbf9b30219167
keunhyeok <hyc7575@naver.com>
2017-04-13 14:42:49 +0900

    array 확장 unique method

--- h2m.js
+++ h2m.js
@@ -152,6 +152,19 @@
             };
         }());
     }
+
+    // Array 확장
+    Array.prototype.unique = function() {
+        var a = this.concat();
+        for( var i = 0; i < a.length; ++i ) {
+            for( var j = i + 1; j < a.length; ++j ) {
+                if( a[i] === a[j] )
+                    a.splice(j--, 1);
+            }
+        }
+
+        return a;
+    };
 })(window);
 
 (function() {

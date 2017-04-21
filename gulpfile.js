c2c2a32061a202fe3457ce18fdd9fbe9085c5031
keunhyeok <hyc7575@naver.com>
2017-04-21 17:20:59 +0900

    gulp task

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var stripDebug = require('gulp-strip-debug');
+var gutil = require('gulp-util');
+var plumber = require('gulp-plumber');
+
+var path = {
+    js: 'src/js/*.js',
+    css: 'src/css/*.css'
+}
+
+gulp.task('watch', function() {
+    gulp.watch(path.js, ['combine-js'])
+});
+gulp.task('combine-js', function() {
+    return gulp.src(path.js)
+        .pipe(plumber(function(err) {
+            gutil.log(err.message);
+        }))
+        .pipe(stripDebug()) // alert, console 제거
+        .pipe(uglify()) // minify
+    	.pipe(concat('h2m.js'))
+    	.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('default', ['combine-js', 'watch']);
